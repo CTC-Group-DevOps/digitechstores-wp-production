4dfe60cf1c273dddd8b2369b7e3e9c070e6273ee
cPanel Auto-Backup (PROD) <munzir.mukhtar@ctcgroupltd.com>
2026-07-20 13:34:32 +0100

    Initial clean commit tracking only custom themes and plugins

---
+++ wp-content/plugins/woocommerce-admin-custom-order-fields/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '55ae17bea8b63b14e536f9c11438d57bf0f7074e',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '55ae17bea8b63b14e536f9c11438d57bf0f7074e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'skyverge/wc-plugin-framework' => array(
+            'pretty_version' => '6.2.0',
+            'version' => '6.2.0.0',
+            'reference' => 'becd6a9127cf050abadb6977530c67cf82531df8',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../skyverge/wc-plugin-framework',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
